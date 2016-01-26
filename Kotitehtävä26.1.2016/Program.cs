42ec7bdaba1b568851099877569be6806d829c55
Jarmo Luostarinen <k3295@student.jamk.fi>
2016-01-26 20:34:00 +0200

    koti

---
+++ Kotitehtävä26.1.2016/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotitehtävä26._1._2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
